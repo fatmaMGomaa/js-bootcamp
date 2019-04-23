7b8c3858f2036aebca5c6af7889a7ab0b38e1019
fatmaMGomaa <fatma-mohamed@feps.edu.eg>
2019-04-23 17:02:26 +0200

    refactor todos array to be array of objects and add remove function

---
+++ arrays/notes.js
@@ -0,0 +1,4 @@
+let item={}
+let otherItem = item
+// let otherItem={}
+console.log(item===otherItem)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,21 +1,25 @@
-const todos=["play with wesky", "study", "do work", "call my daddy"]
-
-// console.log(`you have ${todos.length} todos to do 
-// todo:${todos[0]}
-// todo:${todos[1]}
-// todo:${todos[todos.length-1]}`)
-
-// todos.splice(2,1)
-// console.log(todos)
-// todos.push("hello world")
-// console.log(todos)
-// todos.shift()
-// console.log(todos)
-
-// todos.forEach((todo, index)=>{
-//     console.log(`${index+1}. ${todo} `)
-// })
-
-for(let i=0; i<todos.length;i++){
-    console.log(`${i+1}. ${todos[i]}`)
+const todos=[{
+    text: "walk with wesky",
+    completed: true
+}, {
+    text: "Buy food",
+    completed: false
+}, {
+    text: "do work",
+    completed: true
+}, {
+    text: "study",
+    completed: false
+}, {
+    text: "call my daddy",
+    completed: false
 }
+]
+const removeTodo=function (todos,text) {
+    const todoIndex=todos.findIndex((todo,index)=>{
+        return todo.text.toUpperCase() === text.toUpperCase()
+    })
+    return todoIndex>=0?todos.splice(todoIndex,1):todos
+}
+removeTodo(todos,"buy food")
+console.log(todos)
