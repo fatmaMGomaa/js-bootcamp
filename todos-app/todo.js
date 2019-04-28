a8ced607aefb007f40b2ee6bec83d6c40c0894f0
fatmaMGomaa <fatma-mohamed@feps.edu.eg>
2019-04-29 01:42:09 +0200

    dom/renderingData

--- todos-app/todo.js
+++ todos-app/todo.js
@@ -1,7 +1,36 @@
-const ps=document.querySelectorAll('p')
-const removePThePs= function () {
-    ps.forEach((p)=>{
-        p.textContent.toLocaleLowerCase().includes("the")?p.remove():p
+const todos = [{
+    text: "walk with wesky",
+    completed: true
+}, {
+    text: "Buy food",
+    completed: false
+}, {
+    text: "do work",
+    completed: true
+}, {
+    text: "study",
+    completed: false
+}, {
+    text: "call my daddy",
+    completed: true
+}]
+
+
+const renderingTodos=function (todos) {
+    const notCompletesTodos = todos.filter((todo, index) => !todo.completed )
+    leftTodos = document.createElement('h2')
+    leftTodos.textContent = `you have ${notCompletesTodos.length} left to do`
+    document.querySelector('body').appendChild(leftTodos)
+
+    todos.forEach((todo) => {
+        const newTodo = document.createElement('p')
+        newTodo.textContent = todo.text
+        document.querySelector('body').appendChild(newTodo)
+
     })
 }
-removePThePs()
+renderingTodos(todos)
+
+document.querySelector('button').addEventListener('click',(e)=>{
+    console.log("it's fired")
+})
